0f85edbd2290e80d2a22b35bd0b802405596b092
Ivanova-Ekaterina <ivanova.ev@phystech.edu>
2018-10-15 18:32:32 +0300

    hw3

---
+++ app/main.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+from wsgiref.util import request_uri
+
+
+def application(env, start_resp):
+    start_resp('200 OK', [('Content-Type', 'application/json')])
+    url = request_uri(env)
+    time = str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    test = '{"time":"' + time + '","url": "' + url  + '"}'
+    return [test.encode('utf-8')]
